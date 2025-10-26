ac80f177d3ecfc975d52297f1116f40c531110a4
fedorshvetsov58 <fedor.shvetsov58@gmail.com>
2025-10-26 15:27:16 +0100

    Dodano login JWT i ochronę CRUD za pomocą token_required; testy w Thunder Client

--- app.py
+++ app.py
@@ -2,13 +2,32 @@ from flask import Flask, jsonify, request, render_template
 from database import get_db_connection, init_db
 from models import hash_password
 import bcrypt
-
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
 
 with app.app_context():
     init_db()
+
+SECRET_KEY = "tajny_klucz_do_jwt"
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(" ")[1]
+        if not token:
+            return jsonify({'error': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except:
+            return jsonify({'error': 'Token is invalid or expired!'}), 401
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def home():
@@ -31,6 +50,7 @@ def get_zadanie(id):
     return jsonify(dict(zadanie)), 200
 
 @app.route('/api/zadania', methods=['POST'])
+@token_required
 def create_zadanie():
     data = request.get_json()
     required = ['tytul', 'opis', 'deadline', 'priorytet']
@@ -46,6 +66,7 @@ def create_zadanie():
     return jsonify({'id': cur.lastrowid}), 201
 
 @app.route('/api/zadania/<int:id>', methods=['PUT'])
+@token_required
 def update_zadanie(id):
     data = request.get_json()
     conn = get_db_connection()
@@ -61,6 +82,7 @@ def update_zadanie(id):
     return jsonify({'message': 'Zaktualizowano'}), 200
 
 @app.route('/api/zadania/<int:id>', methods=['DELETE'])
+@token_required
 def delete_zadanie(id):
     conn = get_db_connection()
     zadanie = conn.execute('SELECT * FROM zadania WHERE id=?', (id,)).fetchone()
@@ -91,6 +113,31 @@ def register():
     conn.commit()
     conn.close()
     return jsonify({'message': 'Utworzono konto'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    login = data.get('login')
+    password = data.get('password')
+
+    if not login or not password:
+        return jsonify({'error': 'Nie podano loginu lub hasła'}), 400
+
+    conn = get_db_connection()
+    user = conn.execute('SELECT * FROM users WHERE login=?', (login,)).fetchone()
+    conn.close()
+
+    from models import check_password
+
+    if user is None or not check_password(password, user['hasloHash']):
+        return jsonify({'error': 'Nieprawidłowy login lub hasło'}), 401
+
+    token = jwt.encode({
+        'user_id': user['id'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }, SECRET_KEY, algorithm='HS256')
+
+    return jsonify({'token': token})
 
 if __name__ == '__main__':
     app.run(debug=True)
