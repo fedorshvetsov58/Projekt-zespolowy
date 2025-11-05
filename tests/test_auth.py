76e66db1b0b50d4c74d43ece142744f331b55612
fedorshvetsov58 <fedor.shvetsov58@gmail.com>
2025-11-05 22:05:21 +0100

    Fix indentation in test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,6 +1,5 @@
 import random
 import pytest
-from app import app
 import sys
 import os
 
@@ -8,13 +7,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 from database import init_db
 
+
 @pytest.fixture
 def client():
-        app.config["TESTING"] = True
+    app.config["TESTING"] = True
     with app.app_context():
         init_db()
     with app.test_client() as client:
         yield client
+
 
 def test_register_validation_errors(client):
     # za krotki password
